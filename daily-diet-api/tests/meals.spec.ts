cc3ea29cbe21a3d1329e2cf0de9c5179162bb42f
Gabriele Nakassima <gaby.naka@gmail.com>
2024-01-06 23:22:29 -0300

    test: create and update meals tests

--- daily-diet-api/tests/meals.spec.ts
+++ daily-diet-api/tests/meals.spec.ts
@@ -1,9 +1,12 @@
 import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'child_process'
 
 describe('Meals routes', () => {
   beforeAll(async () => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
     await app.ready()
   })
 
@@ -47,15 +50,15 @@ describe('Meals routes', () => {
     await request(app.server)
       .post('/user')
       .send({
-        name: 'Meal test1',
-        email: 'meal1@test.com',
+        name: 'Meal test2',
+        email: 'meal2@test.com',
         password: '123456',
       })
 
     const authUser = await request(app.server)
       .post('/auth')
       .send({
-        email: 'meal1@test.com',
+        email: 'meal2@test.com',
         password: '123456',
       })
 
@@ -119,7 +122,7 @@ describe('Meals routes', () => {
         authorization: `Bearer ${authUser.body.token}`,
       })
 
-      const mealsList = await request(app.server)
+    const mealsList = await request(app.server)
       .get('/meals')
       .set({
         authorization: `Bearer ${authUser.body.token}`,
@@ -139,5 +142,149 @@ describe('Meals routes', () => {
       })
 
     expect(updatedMeal.statusCode).toEqual(201);
+  })
+
+  it('should be able to get a meal', async () => {
+    await request(app.server)
+      .post('/user')
+      .send({
+        name: 'Meal test1',
+        email: 'meal1@test.com',
+        password: '123456',
+      })
+
+    const authUser = await request(app.server)
+      .post('/auth')
+      .send({
+        email: 'meal1@test.com',
+        password: '123456',
+      })
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        name: "Test new",
+        description: "description new",
+        date: "2023-12-15",
+        hours: "12:00",
+        within_diet: true
+      })
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    const mealsList = await request(app.server)
+      .get('/meals')
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    const getAMeal = await request(app.server)
+      .get(`/meals/${mealsList.body.meals[0].id}`)
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    expect(getAMeal.statusCode).toEqual(200);
+    expect(getAMeal.body.meal).toEqual(
+      expect.objectContaining({
+        id: mealsList.body.meals[0].id,
+        name: "Test Update",
+        description: "description",
+        date: "2023-12-01",
+        hours: "12:00",
+        within_diet: 1
+      }),
+    )
+  })
+
+  it('should be able to delete a meal', async () => {
+    await request(app.server)
+      .post('/user')
+      .send({
+        name: 'Meal test1',
+        email: 'meal1@test.com',
+        password: '123456',
+      })
+
+    const authUser = await request(app.server)
+      .post('/auth')
+      .send({
+        email: 'meal1@test.com',
+        password: '123456',
+      })
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        name: "Test new",
+        description: "description new",
+        date: "2023-12-15",
+        hours: "12:00",
+        within_diet: true
+      })
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    const mealsList = await request(app.server)
+      .get('/meals')
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    const deleteMeal = await request(app.server)
+      .get(`/meals/${mealsList.body.meals[0].id}`)
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    expect(deleteMeal.statusCode).toEqual(200);
+  })
+
+  it('should be able to get metrics', async () => {
+    await request(app.server)
+      .post('/user')
+      .send({
+        name: 'Meal test1',
+        email: 'meal1@test.com',
+        password: '123456',
+      })
+
+    const authUser = await request(app.server)
+      .post('/auth')
+      .send({
+        email: 'meal1@test.com',
+        password: '123456',
+      })
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        name: "Test new",
+        description: "description new",
+        date: "2023-12-15",
+        hours: "12:00",
+        within_diet: true
+      })
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    const metrics = await request(app.server)
+      .get('/meals/metrics')
+      .set({
+        authorization: `Bearer ${authUser.body.token}`,
+      })
+
+    expect(metrics.statusCode).toEqual(200);
+    expect(metrics.body).toEqual(
+      expect.objectContaining({
+        total_meals: 5,
+        total_within_diet: 5,
+        total_without_diet: 0,
+        total_best_sequence: 1
+      }),
+    )
   })
 })
